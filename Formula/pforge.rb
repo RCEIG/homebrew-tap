12871e983e53f401e0a83107177b819194640cf5
Noah Houser <noahhouser@noahs.mac.mini.lan>
2026-04-18 14:27:50 -0500

    chore: bump pforge formula to v1.3.0 (SHAs pending CI build)

--- Formula/pforge.rb
+++ Formula/pforge.rb
@@ -3,26 +3,26 @@
 class Pforge < Formula
   desc "AI-powered pipeline generator - create CI/CD workflows in seconds"
   homepage "https://pipelineforge.dev"
-  version "1.2.2"
+  version "1.3.0"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/RCEIG/PipelineForge-Product/releases/download/v#{version}/pforge-darwin-arm64.tar.gz"
-      sha256 "cbc9a4ce974c3f8785b912ac6cbeca9aff0bf0fe0a09513218324ea3ad16ff4a"
+      sha256 "PLACEHOLDER_DARWIN_ARM64_SHA256"
     else
       url "https://github.com/RCEIG/PipelineForge-Product/releases/download/v#{version}/pforge-darwin-amd64.tar.gz"
-      sha256 "f973670cbe381f7d26b95fabbe676e4f85fd6021bc01edeaddf9298f81e926c9"
+      sha256 "PLACEHOLDER_DARWIN_AMD64_SHA256"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/RCEIG/PipelineForge-Product/releases/download/v#{version}/pforge-linux-arm64.tar.gz"
-      sha256 "436f26633b2baa84beef96076d7fee1fd347984a8a716dbb047b002c18a53f16"
+      sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
     else
       url "https://github.com/RCEIG/PipelineForge-Product/releases/download/v#{version}/pforge-linux-amd64.tar.gz"
-      sha256 "f95eb109af8254d3d0a868de4d131b40f8261fb2d66ae11cebd06690c11488bd"
+      sha256 "PLACEHOLDER_LINUX_AMD64_SHA256"
     end
   end
 
