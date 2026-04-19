46f991b999f10a17809e19849c6f715bb5103151
Noah Houser <noahhouser@noahs.mac.mini.lan>
2026-04-19 07:39:42 -0500

    fix: sync Formula/pforge.rb to v1.3.0 (root formula was already updated)

--- Formula/pforge.rb
+++ Formula/pforge.rb
@@ -3,26 +3,26 @@
 class Pforge < Formula
   desc "AI-powered pipeline generator - create CI/CD workflows in seconds"
   homepage "https://pipelineforge.dev"
-  version "1.2.9"
+  version "1.3.0"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/RCEIG/PipelineForge-Product/releases/download/v#{version}/pforge-darwin-arm64.tar.gz"
-      sha256 "7927f98f0a4927b49057be599cbbc3952c7d0a6fd267f875139dbc932bd6da31"
+      sha256 "63e3682c17439c178bfd4bbdbd42df58020163c8ac9e2f5f6fb9a02ae891e4e2"
     else
       url "https://github.com/RCEIG/PipelineForge-Product/releases/download/v#{version}/pforge-darwin-amd64.tar.gz"
-      sha256 "b986ae893929482361a71c7506934845347c174cfc318ad678c7f1df13928271"
+      sha256 "2b7f48170583fa1f4c082f1131e21e7d063617c8447062558b3dd121d851f49a"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/RCEIG/PipelineForge-Product/releases/download/v#{version}/pforge-linux-arm64.tar.gz"
-      sha256 "336d2ec4f434be8c1459d91dafd8b9675cd6155ab2b8c918e41c6470fb48a811"
+      sha256 "9bee1145e0a34e50ef96e1cf608129a72315cb1337777a155cdfb54612b50dc6"
     else
       url "https://github.com/RCEIG/PipelineForge-Product/releases/download/v#{version}/pforge-linux-amd64.tar.gz"
-      sha256 "258cba77ddee77fdb57a243e28a0a6b36f77a1a37c0abc8ea1e1f24229f753a0"
+      sha256 "8c07c252e47b34bbb882a4f53d5bcd9b27f37f0f12366f152fc68bea0af99a7f"
     end
   end
 
